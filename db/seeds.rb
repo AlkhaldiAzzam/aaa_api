850f7bb7cb28efff706004883257f7aa8e5ffd6a
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2019-08-20 12:17:34 +0300

    Stoooooooory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Scene.create(scene_text: 'bhvrkgdskjgf' , left_text: 'hfeygw' , right_text: 'jcgewj')
-Scene.create(scene_text: 'bhvrkgdskjgf' , left_text: 'hfeygw' , right_text: 'jcgewj')
-Scene.create(scene_text: 'bhvrkgdskjgf' , left_text: 'hfeygw' , right_text: 'jcgewj')
+Scene.destroy_all
+Scene.create(scene_text: 'You woke up in a forest with a strong headache. You reach for your weapon but it’s not there! You look around you to find a way out of this forest and you see 2 paths: one is open and there is trails of people on it. The other dark and full of thorns, but you can see something shining in the distance. Where would you go?' , left_text: 'Through the open path' , right_text: 'Through the thorny path')
